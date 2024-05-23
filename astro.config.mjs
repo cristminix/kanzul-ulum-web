80ff47940effb490fdff39a44711602458b3c472
Putra Budiman <cristminix@gmail.com>
2024-05-23 17:34:20 +0700

    apply green-theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 
 // import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-
-	integrations: [],
-	build: {
-		assets: "assets-pages",
-	},
+  integrations: [tailwind()],
+  build: {
+    assets: "assets-pages"
+  }
 });
